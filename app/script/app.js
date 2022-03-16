72b1699a17da92073e1baefe8b826a7932f54fc5
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-03-16 01:31:06 +0100

    Keep header logic for reference

--- app/script/app.js
+++ app/script/app.js
@@ -1,7 +1,39 @@
-document.addEventListener('click', event => {
-	const clicked = rule => event.target.closest(rule);
+const $select = (selector) => document.querySelector(selector);
+const $$select = (selector) => document.querySelectorAll(selector);
+const $on = (selector, event, handler) => document.querySelector(selector).addEventListener(event, handler);
+const $$on = (selector, event, handler) => document.querySelectorAll(selector).addEventListener(event, handler);
+const $window = (event, handler) => window.addEventListener(event, handler);
 
-	if (clicked('header')) {
-		console.log('clicked the header');
+function header_component() {
+	let navigation_visible = false;
+
+	function toggle_navigation(event) {
+		navigation_visible = !navigation_visible;
 	}
-});
+
+	function hide_navigation() {
+		navigation_visible = false;
+	}
+
+	function render() {
+		$select('nav').dataset.visible = navigation_visible;
+	}
+
+	$on('.header__menu', 'click', event => {
+		toggle_navigation();
+		render();
+	});
+
+	return {
+		toggle_navigation,
+		hide_navigation,
+		render
+	}
+}
+
+const header = header_component();
+
+setTimeout(() => {
+	header.hide_navigation();
+	header.render();
+}, 5000);
