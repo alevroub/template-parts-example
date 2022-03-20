3baa7607a11bbbe1fd7c61e8e28534753f6208e5
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-03-20 21:29:48 +0100

    Replace layout functions with custom resolver

--- server/server.ts
+++ server/server.ts
@@ -7,9 +7,13 @@ import config from '../api/config.ts';
 const { origin, port } = config;
 const server = new abc();
 
-server.static('/assets', '/app/assets');
-server.static('/style', '/app/style');
-server.static('/script', '/app/script');
+eta.configure({
+	views: 'frontend/components/'
+})
+
+server.static('/assets', '/frontend/assets');
+server.static('/style', '/frontend/style');
+server.static('/script', '/frontend/script');
 
 for (const route of routes) {
 	const { path, component, controller } = route;
@@ -17,9 +21,13 @@ for (const route of routes) {
 	server.get(path, async ({ request, response }) => {
 		const { head, data } : any = await controller(request);
 
-		return eta.renderFile(
-			absolute_path(`../app/pages/${component}`), { request, response, head, data }
-		);
+		const page_file = await Deno.readTextFile(`frontend/pages/${component}`);
+		const app_file = await Deno.readTextFile('frontend/components/app.eta');
+
+		const page = eta.render(page_file, { request, head, data });
+		const app = eta.render(app_file, { request, head, data, page });
+
+		return app;
 	});
 }
 
