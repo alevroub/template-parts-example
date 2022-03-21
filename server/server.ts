d6e83c8cce1845a74e24a5272d6de01d9a684e74
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-03-21 14:34:41 +0100

    Move handlers to separate functions, add CORS example

--- server/server.ts
+++ server/server.ts
@@ -16,7 +16,7 @@ server.static('/style', '/frontend/style');
 server.static('/script', '/frontend/script');
 
 for (const route of routes) {
-	server.get(route.path, async ({ request, response }) => {
+	async function get_handler({ request, response }) {
 		const { head, data } : any = await route.controller(request);
 
 		const page_file = await Deno.readTextFile(`frontend/pages/${route.page}`);
@@ -26,7 +26,30 @@ for (const route of routes) {
 		const app = eta.render(app_file, { request, head, data, page });
 
 		return app;
-	});
+	}
+
+	async function post_handler({ request, response }) {
+		const { data } : any = await route.controller(request);
+		return data;
+	}
+
+	function cors_middleware(next) {
+		return (context) => {
+			const { request, response } = context;
+			const allowed = ['http://localhost:3000'];
+			const origin = request.headers.get('origin');
+
+			if (origin && allowed.includes(origin)) {
+				return next(context);
+			} else {
+				response.status = 403;
+				response.body = 'Forbidden';
+			}
+		}
+	}
+
+	server.get(route.path, get_handler);
+	server.post(route.path, post_handler, cors_middleware)
 }
 
 server.start({ port });
