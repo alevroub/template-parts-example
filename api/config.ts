18188d0cc6f486469538bd1dbe5c4a85587f2293
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-03-21 14:52:04 +0100

    Always allow current origin

--- api/config.ts
+++ api/config.ts
@@ -2,6 +2,6 @@ import { development_mode } from '../server/util.ts'
 
 export default {
 	port: 3000,
-	origin: development_mode ? 'http://localhost' : 'http://production.mode',
-	allowed_origins: ['http://localhost:3000']
+	origin: development_mode ? 'http://localhost:3000' : 'http://production.mode',
+	origins_allowed: []
 }
--- server/middleware.ts
+++ server/middleware.ts
@@ -2,8 +2,9 @@ function cors(next) {
 	return context => {
 		const { request, response } = context;
 		const origin = request.headers.get('origin');
+		const origins_allowed = [config.origin, ...config.origins_allowed]
 
-		if (origin && config.allowed_origins.includes(origin)) {
+		if (origin && origins_allowed.includes(origin)) {
 			return next(context);
 		} else {
 			response.status = 403;
