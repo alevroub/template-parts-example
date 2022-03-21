7adce7a20440dcbb1854bd20ceef66f844329788
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-03-21 14:39:43 +0100

    Keep allowed origins in config file

--- api/config.ts
+++ api/config.ts
@@ -2,5 +2,6 @@ import { development_mode } from '../server/util.ts'
 
 export default {
 	port: 3000,
-	origin: development_mode ? 'http://localhost' : 'http://production.mode'
+	origin: development_mode ? 'http://localhost' : 'http://production.mode',
+	allowed_origins: ['http://localhost:3000']
 }
