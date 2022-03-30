8795a71c2631217c2a0c5bb776be787395ca3135
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-03-30 14:09:50 +0200

    Syntax formatting

--- api/controllers.ts
+++ api/controllers.ts
@@ -1,15 +1,11 @@
-import {sanity_fetch} from '../server/sanity.ts'
+import { sanity_fetch } from '../server/sanity.ts';
 
 export async function index(request: any) {
 	const query = `*[slug.current == $slug][0]`;
 	const params = { slug: 'alfa' };
 
-	const data : any = await sanity_fetch(query, params);
+	const data: any = await sanity_fetch(query, params);
+	const head: any = { title: 'PAGE TITLE', lang: 'no' };
 
-	const head : any = {
-		title: "PAGE TITLE",
-		lang: 'no',
-	}
-
-	return { head, data }
+	return { head, data };
 }
