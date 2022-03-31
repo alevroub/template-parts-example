9053a76e1473145b58a89b5f73804f8cdf98b80a
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-03-31 12:35:02 +0200

    Add more page examples

--- api/routes.ts
+++ api/routes.ts
@@ -1,5 +1,7 @@
-import { index } from './controllers.ts'
+import { index, page } from './controllers.ts'
 
 export default [
+	{ path: '/:page_id', page: 'page.eta', controller: page },
 	{ path: '/', page: 'index.eta', controller: index },
+	{ path: '*', page: '404.eta' },
 ];
