b85f3fb9b1fbe428f5fc2e3fe8f42bcc9e65f9b1
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-01 13:45:49 +0200

    Temporary port change

--- api/config.ts
+++ api/config.ts
@@ -1,7 +1,7 @@
 import { development_mode } from '../server/util.ts'
 
 export default {
-	port: 80,
+	port: 8000,
 	origin: development_mode ? 'http://localhost:3000' : 'http://production.mode',
 	origins_allowed: [],
 	sanity: {
