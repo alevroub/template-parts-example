cbb629787f89de7e0f06c98a27b8259d0b79a8e9
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-01 13:47:45 +0200

    Update port and origin for deno deploy

--- api/config.ts
+++ api/config.ts
@@ -1,8 +1,8 @@
 import { development_mode } from '../server/util.ts'
 
 export default {
-	port: 8000,
-	origin: development_mode ? 'http://localhost:3000' : 'http://production.mode',
+	port: 80,
+	origin: development_mode ? 'http://localhost:3000' : 'https://alejandro.deno.dev',
 	origins_allowed: [],
 	sanity: {
 		id: "32td7jzv",
