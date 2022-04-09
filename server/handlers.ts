23d1f36cbf0b5fb73deba8a03169742ff18c117e
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-09 23:58:15 +0200

    Tweak request handlers, return response object

--- server/handlers.ts
+++ server/handlers.ts
@@ -4,7 +4,7 @@ eta.configure({
 	views: 'frontend/components/',
 });
 
-export async function get_handler({ request, response }, route) {
+export async function get_handler({ request, params }, { route }) {
 	if (!route.controller) {
 		route.controller = () => ({ head: {}, data: {} });
 	}
@@ -14,14 +14,18 @@ export async function get_handler({ request, response }, route) {
 	const page_file = await Deno.readTextFile(`frontend/pages/${route.page}`);
 	const app_file = await Deno.readTextFile('frontend/components/app.eta');
 
-	const page = eta.render(page_file, { request, response, head, data });
-	const app = eta.render(app_file, { request, response, head, data, page });
+	const page = eta.render(page_file, { request, head, data });
+	const app = eta.render(app_file, { request, head, data, page });
 
-	response.body = app;
+	return new Response(app, {
+		headers: { 'content-type': 'text/html' }
+	});
 }
 
-export async function post_handler({ request, response }, route) {
+export async function post_handler({ request, params }, { route }) {
 	const { data }: any = await route.controller(request);
 
-	response.body = data;
+	return new Response(data, {
+		headers: { 'content-type': 'application/json' }
+	});
 }
