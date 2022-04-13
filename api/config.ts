7915682eaf63cb6d446fc5ea2c0d821cb0c1af1b
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-13 19:56:54 +0200

    Use getters for reusable properties

--- api/config.ts
+++ api/config.ts
@@ -1,15 +1,20 @@
-import { development_mode } from '../server/util.ts'
-
-const port = development_mode ? 3000 : 80;
+import { development_mode } from '../server/util.ts';
 
 export default {
-	port: port,
-	origin: development_mode ? `http://localhost:${port}` : 'https://alejandro.deno.dev',
-	origins_allowed: [],
+	port: development_mode ? 3000 : 80,
+
+	get origin() {
+		return development_mode ? `http://localhost:${this.port}` : 'https://alejandro.deno.dev';
+	},
+
+	get origins_allowed() {
+		return [this.origin];
+	},
+
 	sanity: {
-		id: "32td7jzv",
+		id: '32td7jzv',
 		version: '2022-03-01',
 		dataset: 'production',
-		cdn: true
-	}
-}
+		cdn: true,
+	},
+};
