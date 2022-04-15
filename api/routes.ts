3e7012dd03e78442307dab25676c79cb8fddcd05
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-15 18:28:43 +0200

    Use regex catch all rule

--- api/routes.ts
+++ api/routes.ts
@@ -3,5 +3,5 @@ import { index, page } from './controllers.ts';
 export default [
 	{ path: '/:page_id', page: 'page.html', controller: page },
 	{ path: '/', page: 'index.html', controller: index },
-	{ path: '*', page: '404.html' },
+	{ path: '(.*)', page: '404.html' },
 ];
