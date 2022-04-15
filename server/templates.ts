e45ee314a09c3de4d7b489f35b930c7fe36c7c4b
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-15 18:29:39 +0200

    Export templates as a function to be run on demand

--- server/templates.ts
+++ server/templates.ts
@@ -1,8 +1,12 @@
-const template_main: string = await Deno.readTextFile('frontend/app/template.html');
-const template_pages: Record<string, string> = {};
+async function read_templates() {
+	const template_main: string = await Deno.readTextFile('frontend/app/template.html');
+	const template_pages: Record<string, string> = {};
 
-for await (const page of Deno.readDir('frontend/pages')) {
-	template_pages[page.name] = await Deno.readTextFile(`frontend/pages/${page.name}`);
+	for await (const page of Deno.readDir('frontend/pages')) {
+		template_pages[page.name] = await Deno.readTextFile(`frontend/pages/${page.name}`);
+	}
+
+	return { template_main, template_pages };
 }
 
-export { template_main, template_pages };
+export { read_templates };
