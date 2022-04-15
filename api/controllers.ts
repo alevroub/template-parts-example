ab97695ae845f2555c9b3a3b3ca39443700666eb
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-15 21:00:54 +0200

    Add more data to controllers

--- api/controllers.ts
+++ api/controllers.ts
@@ -1,19 +1,32 @@
-import { Context } from '../server/types.ts';
+import { RouteContext } from '../server/types.ts';
 import { sanity_fetch } from '../server/sanity.ts';
 
-export async function index(context: Context) {
+export async function home() {
 	const query = `*[slug.current == $slug][0]`;
 	const query_params = { slug: 'alfa' };
 
-	const data = await sanity_fetch(query, query_params);
-	const meta = { title: 'HOMEPAGE' };
+	const data = {
+		description: 'This comes from controllers.ts. Returns data from sanity.',
+		from_sanity: await sanity_fetch(query, query_params)
+	}
+
+	const meta = {
+		title: 'STATIC TEST'
+	};
 
 	return { data, meta };
 }
 
-export async function page(context: Context) {
-	const data = { numbers: [1, 2, 3, 4] };
-	const meta = { title: 'PAGE TITLE' };
+export async function page(context: RouteContext) {
+	const data = {
+		description: 'This comes from controllers.ts. Returns a static object.',
+		params: context.params,
+		numbers: [1, 2, 3, 4]
+	};
+
+	const meta = {
+		title: 'PAGE – STATIC TEST'
+	};
 
 	return { data, meta };
 }
