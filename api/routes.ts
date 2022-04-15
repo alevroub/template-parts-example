1183e7ec3a786880bcfb768f5bd8662321884577
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-15 21:01:13 +0200

    Keep routes unformatted for readability

--- api/routes.ts
+++ api/routes.ts
@@ -1,7 +1,7 @@
-import { index, page } from './controllers.ts';
+import { home, page } from './controllers.ts';
 
 export default [
-	{ path: '/:page_id', page: 'page.html', controller: page },
-	{ path: '/', page: 'index.html', controller: index },
-	{ path: '(.*)', page: '404.html' },
+	{ path: '/:page_id', page: 'page.html', 	controller: page 	},
+	{ path: '/', 			page: 'home.html', 	controller: home 	},
+	{ path: '(.*)', 		page: '404.html' 								},
 ];
