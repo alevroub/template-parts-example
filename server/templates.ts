371b92980a63b55ec56f118b903d2cabefca4a13
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-16 02:02:28 +0200

    Rename main template file

--- server/templates.ts
+++ server/templates.ts
@@ -1,5 +1,5 @@
 async function read_templates() {
-	const template_main: string = await Deno.readTextFile('frontend/app/template.html');
+	const template_main: string = await Deno.readTextFile('frontend/app/app.html');
 	const template_pages: Record<string, string> = {};
 
 	for await (const page of Deno.readDir('frontend/pages')) {
