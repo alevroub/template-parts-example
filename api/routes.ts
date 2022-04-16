53db593fb231d26f73523d2d13b644af81d2cd81
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-16 02:01:37 +0200

    Prefix catch all path

--- api/routes.ts
+++ api/routes.ts
@@ -3,5 +3,5 @@ import { home, page } from './controllers.ts';
 export default [
 	{ path: '/:page_id', page: 'page.html', 	controller: page 	},
 	{ path: '/', 			page: 'home.html', 	controller: home 	},
-	{ path: '(.*)', 		page: '404.html' 								},
+	{ path: '/(.*)', 		page: '404.html' 								},
 ];
