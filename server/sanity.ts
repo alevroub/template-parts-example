c7477db2c3a4605e1197ff8f09e6b18d60569898
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-17 20:44:03 +0200

    Move query logic to separate function

--- server/sanity.ts
+++ server/sanity.ts
@@ -2,19 +2,23 @@ import { config } from './internal.ts';
 import { in_development_mode } from './util.ts';
 import { get_from_cache, store_in_cache } from '../server/cache.ts';
 
-export async function sanity_fetch(query: string, params: Record<string, any>): Promise<any> {
+export async function sanity_query(query: string, params: Record<string, any>): Promise<any> {
 	const { id, version, dataset, cdn } = config.sanity;
 
 	const host = cdn === true ? 'apicdn.sanity.io' : 'api.sanity.io';
 	const encoded_query = encodeURIComponent(query);
-	const encoded_params = Object.keys(params).reduce(
-		(sequence, key) =>
-			sequence + `&${encodeURIComponent(`$${key}`)}=${encodeURIComponent(JSON.stringify(params[key]))}`,
-		''
-	);
+	const encoded_params = Object.keys(params).reduce((sequence, key) => {
+		return sequence + `&${encodeURIComponent(`$${key}`)}=${encodeURIComponent(JSON.stringify(params[key]))}`;
+	}, '');
+
 	const request_url = `https://${id}.${host}/v${version}/data/query/${dataset}?query=${encoded_query}${encoded_params}`;
+	const response = await fetch(request_url);
+	const { result } = await response.json();
 
+	return result;
+}
 
+export async function sanity_fetch(query: string, params: Record<string, any>): Promise<any> {
 	if (in_development_mode) {
 		const cached_result = await get_from_cache(query);
 
@@ -23,8 +27,7 @@ export async function sanity_fetch(query: string, params: Record<string, any>): 
 		}
 	}
 
-	const response = await fetch(request_url);
-	const { result } = await response.json();
+	const result = await sanity_query(query, params);
 
 	if (in_development_mode) {
 		store_in_cache(query, result);
