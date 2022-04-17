5a3ea48b27469f12280195f598616406a380eb9d
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-18 01:17:05 +0200

    Syntax formatting

--- server/cache.ts
+++ server/cache.ts
@@ -8,7 +8,7 @@ async function return_hash(input: string) {
 	const text_encoder = new TextEncoder();
 	const hash_buffer = await crypto.subtle.digest('SHA-1', text_encoder.encode(input));
 	const hash_array = Array.from(new Uint8Array(hash_buffer));
-	const hash_hex = hash_array.map(b => b.toString(16).padStart(2, '0')).join('');
+	const hash_hex = hash_array.map(value => value.toString(16).padStart(2, '0')).join('');
 
 	return hash_hex;
 }
