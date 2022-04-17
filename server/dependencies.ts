b639c0100c9c9806d954f54162ca140c1894f0b7
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-18 01:03:33 +0200

    Get nano from deno.land

--- server/dependencies.ts
+++ server/dependencies.ts
@@ -1,5 +1,5 @@
 import { Application, Router, Status, Context, Request, Response } from 'https://deno.land/x/oak/mod.ts';
-import { render } from 'https://raw.githubusercontent.com/alevroub/nano-template-engine/main/mod.ts';
+import { render } from 'https://deno.land/x/nano/mod.ts';
 import { lookup as mimetype } from 'https://deno.land/x/media_types/mod.ts';
 import { parse as parse_flags } from 'https://deno.land/std/flags/mod.ts';
 import { debounce } from 'https://deno.land/std/async/mod.ts';
