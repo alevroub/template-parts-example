e25efbaea5326e340a121c347ccd0a7251acab42
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-04-17 19:31:11 +0200

    Cache controller results

--- server/dependencies.ts
+++ server/dependencies.ts
@@ -1,8 +1,9 @@
 import { Application, Router, Status, Context, Request, Response } from "https://deno.land/x/oak/mod.ts";
+import { render } from 'https://raw.githubusercontent.com/alevroub/nano-template-engine/main/mod.ts';
 import { lookup as mimetype } from 'https://deno.land/x/media_types/mod.ts';
 import { parse as parse_flags } from 'https://deno.land/std/flags/mod.ts';
 import { debounce } from "https://deno.land/std/async/mod.ts";
-import { render } from 'https://raw.githubusercontent.com/alevroub/nano-template-engine/main/mod.ts';
+import { ensureDir as ensure_dir, ensureFile as ensure_file } from "https://deno.land/std/fs/mod.ts";
 
 const process = parse_flags(Deno.args);
 const oak_server = Application;
@@ -20,5 +21,7 @@ export {
 	render,
 	mimetype,
 	process,
-	debounce
+	debounce,
+	ensure_dir,
+	ensure_file
 };
--- server/sanity.ts
+++ server/sanity.ts
@@ -1,4 +1,6 @@
 import { config } from './internal.ts';
+import { development_mode } from './util.ts';
+import { get_from_cache, store_in_cache } from '../server/cache.ts';
 
 export async function sanity_fetch(query: string, params: Record<string, any>): Promise<any> {
 	const { id, version, dataset, cdn } = config.sanity;
@@ -8,8 +10,16 @@ export async function sanity_fetch(query: string, params: Record<string, any>): 
 	const encoded_params = Object.keys(params).reduce((sequence, key) => sequence + `&${encodeURIComponent(`$${key}`)}=${encodeURIComponent(JSON.stringify(params[key]))}`, '');
 	const request_url = `https://${id}.${host}/v${version}/data/query/${dataset}?query=${encoded_query}${encoded_params}`;
 
-	const response = await fetch(request_url);
-	const { result } = await response.json();
+	const cached_result = await get_from_cache(query);
 
-	return result;
+	if (development_mode && cached_result) {
+		return cached_result;
+	} else {
+		const response = await fetch(request_url);
+		const { result } = await response.json();
+
+		store_in_cache(query, result);
+
+		return result;
+	}
 }
