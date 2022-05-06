deda49c1ac9003f538a29bc8dcd1a6e7321a516a
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-06 14:33:33 +0200

    Add command for cache busting

--- run.sh
+++ run.sh
@@ -11,6 +11,10 @@ case $1 in
 		deno run --allow-net --allow-read --allow-write --watch server/server.ts --mode development
 	;;
 
+	'cache')
+		deno cache --reload server/server.ts
+	;;
+
 	'build')
 		esbuild --bundle app/style/app.css --outdir=$build_directory --minify &&
 		esbuild --bundle app/script/app.js --outdir=$build_directory --minify
