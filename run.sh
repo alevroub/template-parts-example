ecca8092b55e51de4c20e0ae851573b50983f940
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-06 17:19:46 +0200

    Simplify run script

--- run.sh
+++ run.sh
@@ -1,22 +1,7 @@
 #!/bin/zsh
 
-build_directory=frontend/assets/
-
 case $1 in
 	'')
 		deno run --no-check --allow-net --allow-read --allow-write --watch server/server.ts --mode development
-	;;
-
-	'check')
-		deno run --allow-net --allow-read --allow-write --watch server/server.ts --mode development
-	;;
-
-	'cache')
-		deno cache --reload server/server.ts
-	;;
-
-	'build')
-		esbuild --bundle app/style/app.css --outdir=$build_directory --minify &&
-		esbuild --bundle app/script/app.js --outdir=$build_directory --minify
 	;;
 esac
