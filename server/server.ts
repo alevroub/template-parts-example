82b55383b1c05311362dfb166dfcab45c9d1dd24
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-06 17:49:35 +0200

    Short url

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import { new_server } from 'https://raw.githubusercontent.com/alevroub/nett/main/mod.ts';
+import { new_server } from 'https://server.boing.link';
 
 const config = {};
 const routes = [
