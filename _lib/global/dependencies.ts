de361092ef53643bdf6a752bc232ec379f730b1e
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-06 14:30:57 +0200

    Update nano

--- _lib/global/dependencies.ts
+++ _lib/global/dependencies.ts
@@ -1,5 +1,5 @@
 import { Application, Router, Status, Context, Request, Response } from 'https://deno.land/x/oak@v10.5.1/mod.ts';
-import { render } from 'https://deno.land/x/nano@v0.0.4/mod.ts';
+import { render } from 'https://deno.land/x/nano@v0.0.5/mod.ts';
 import { debounce } from 'https://deno.land/std@0.137.0/async/mod.ts';
 import { join as path_join, parse as path_parse } from "https://deno.land/std@0.137.0/path/mod.ts";
 import { parse as parse_flags } from 'https://deno.land/std@0.137.0/flags/mod.ts';
