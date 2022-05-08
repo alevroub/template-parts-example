298f28229a02ef29ebcbeb373f7b6bd66a0a6dc3
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-08 03:51:35 +0200

    Add check and cache commands

--- run.sh
+++ run.sh
@@ -4,4 +4,12 @@ case $1 in
 	'')
 		deno run --no-check --allow-net --allow-read --allow-write --watch server/server.ts --mode development
 	;;
+
+	'check')
+		deno run --allow-net --allow-read --allow-write --watch server/server.ts --mode development
+	;;
+
+	'cache')
+		deno cache --reload --no-check server/server.ts
+	;;
 esac
