3124cae233e5330f40082b3f76e7bf9137a1741d
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-12 15:38:16 +0200

    Remove 404

--- server/routes.ts
+++ server/routes.ts
@@ -2,10 +2,6 @@ import { home, page } from './controllers.ts';
 
 export default [
 	{
-		path: '/404',
-		page: '404.html',
-	},
-	{
 		path: '/:project_slug',
 		page: 'page.html',
 		controller: page,
